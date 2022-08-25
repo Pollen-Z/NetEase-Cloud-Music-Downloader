d70e2ee4ad055c16a313def303c099b0c77e86de
HaoranZing <98813410+Pollen-Z@users.noreply.github.com>
2022-08-25 17:36:17 +0800

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-document.querySelector(".down").addEventListener("click", function () {
-    let url = document.querySelector("input").value;
-    let id = url.split("id=")[1].split("&")[0];
-    let musicUrl = `http://music.163.com/song/media/outer/url?id=${id}.mp3`;
-    window.open(musicUrl);
-})
